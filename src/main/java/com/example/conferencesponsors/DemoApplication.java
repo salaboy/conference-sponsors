3a5c0c761a9a3f703d88059ec39e3fc2832c196f
salaboy <salaboy@gmail.com>
2019-08-03 11:35:04 +0100

    adding version

--- src/main/java/com/example/conferencesponsors/DemoApplication.java
+++ src/main/java/com/example/conferencesponsors/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.conferencesponsors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Value("${version:0}")
+	private String version;
+
+	@GetMapping
+	public String helloSponsorsWithVersion() {
+		return "SPONSORS v" + version;
+	}
 }
